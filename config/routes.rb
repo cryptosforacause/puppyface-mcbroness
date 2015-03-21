4e7b645981d39b612199f961561c0be9fb8cafff
cryptosforacause <loopholemaster@gmail.com>
2015-03-21 20:34:51 +0530

    Finishes first iteration of website

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'orders#show'
+  # root 'orders#show'
+
+  get '/' => 'orders#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
